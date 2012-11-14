91e3c5fac9e0468fa5e5449b00477373c21b039b
Jeremy Green <jeremy@octolabs.com>
2012-11-14 00:47:39 -0600

    Starting on iframe bookmarklet.

---
+++ lib/generators/easymarklet/iframe/iframe_generator.rb
@@ -0,0 +1,27 @@
+module Easymarklet
+  class IframeGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+
+    def copy_files
+      template "iframe_bookmarklet.js", "app/assets/javascripts/#{file_name}_bookmarklet.js"
+      template "iframe_consumer.js", "app/assets/javascripts/#{file_name}_consumer.js"
+      template "iframe_producer.js", "app/assets/javascripts/#{file_name}_producer.js"
+      template "iframe_producer_controller.rb", "app/controllers/#{file_name}_producer_controller.rb"
+      template "iframe_producer_index.html.erb", "app/views/#{file_name}_producer/index.html.erb"
+      template "views/easymarklet_layout.html.erb", "app/views/layouts/#{file_name}_producer.html.erb"
+    end
+
+    def create_routes
+      route("match '#{file_name}' => '#{file_name}_producer#index'")
+    end
+
+    def display_msg
+      puts ""
+      puts "You can link to your new bookmarklet with this :"
+      puts ""
+      puts "<%= link_to '#{file_name.titleize}', bookmarklet_js('#{file_name}_consumer.js') %>"
+      puts ""
+    end
+
+  end
+end
---
+++ lib/generators/easymarklet/iframe/templates/iframe_producer_controller.rb
@@ -0,0 +1,8 @@
+class <%= name.camelize %>ProducerController < ApplicationController
+
+  layout '<%= name %>_producer'
+
+  def index
+  end
+
+end
